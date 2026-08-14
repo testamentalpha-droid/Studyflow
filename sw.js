5f231a9e392b0eeb8e949bd2639c3714954240fd
testamentalpha-droid <testamentalpha@gmail.com>
2026-08-14 18:29:14 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE="study-progress-ultimate-v1.1";
+const ASSETS=["./","./index.html","./manifest.json"];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(
+    caches.open(CACHE).then(cache=>cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
+  const url=new URL(event.request.url);
+  if(url.pathname.endsWith("/") || url.pathname.endsWith("/index.html")){
+    event.respondWith(
+      fetch(event.request)
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+          return response;
+        })
+        .catch(()=>caches.match(event.request))
+    );
+  }else{
+    event.respondWith(
+      caches.match(event.request).then(cached=>cached||fetch(event.request))
+    );
+  }
+});
